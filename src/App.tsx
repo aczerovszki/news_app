fa6efbc5df438acacf137548b023b2c2a15d052e
Adrian Czerovszki <aczerovszki@aczerovszki-mbp.3amlabs.net>
2021-05-14 17:30:52 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Card } from "./components/Card";
+import styled from "styled-components";
 
-function App() {
+const App = () => {
+  const [sources, setSources] = useState([]);
+  const url =
+    "https://newsapi.org/v2/everything?" +
+    "q=Apple&" +
+    "from=2021-05-05&" +
+    "sortBy=popularity&" +
+    "apiKey=8f916adb954c4f1194294d4b552bf852";
+
+  const getSources = async () => {
+    const res = await fetch(url);
+    const data = await res.json();
+    const articles = data.articles;
+    console.log(articles);
+    setSources(articles);
+  };
+
+  useEffect(() => {
+    getSources();
+  }, []);
+
+  const Header = styled.h1`
+    text-align: center;
+    margin: 1em;
+  `;
+  const Container = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
+    grid-gap: 2em;
+  `;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header>Latest Apple News</Header>
+      <Container>
+        {sources.map((data) => {
+          return <Card data={data} />;
+        })}
+      </Container>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Card.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Card = ({ data }: any) => {
+  const CardBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    border: 1px solid #ccc;
+    box-shadow: 2px 2px 6px 0px rgb(0 0 0 / 30%);
+    border-radius: 20px;
+  `;
+
+  const CardHeader = styled.div`
+    margin: 0.5em 1.2em;
+    display: flex;
+    justify-content: space-between;
+    gap: 0.5em;
+
+    p {
+      padding: 0.3em;
+      height: fit-content;
+      background-color: darkgrey;
+      border-radius: 0.5em;
+    }
+  `;
+
+  const CardImage = styled.img`
+    max-width: 100%;
+  `;
+
+  const CardText = styled.p`
+    margin: 1em;
+    font-size: 18px;
+    text-align: justify;
+    line-height: 23px;
+  `;
+  const CardButton = styled.a`
+    :hover {
+      background: repeating-linear-gradient(180deg, #404040, #000000 100px);
+      box-shadow: 2px 2px 6px 0px rgb(0 0 0 / 30%);
+    }
+    padding: 0.7em 1.2em;
+    border-radius: 1em;
+    margin: 0 1.3em 1.3em;
+    border: none;
+    text-align: center;
+    color: #fff;
+    text-decoration: none;
+    transition: 0.3s;
+    background: repeating-linear-gradient(180deg, #404040, #000000 100px);
+  `;
+
+  return (
+    <CardBox>
+      <CardHeader>
+        <h2>{data.title}</h2>
+        <p>{new Date(data.publishedAt).toLocaleDateString()}</p>
+      </CardHeader>
+      <CardImage src={data.urlToImage} alt="" />
+      <CardText>{data.description}</CardText>
+      <CardButton href={data.url} target="_blank">
+        Read More
+      </CardButton>
+    </CardBox>
+  );
+};
